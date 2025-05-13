32bf96be0b087b04cf7eadb7fd7b6da842a6441f
Jorge Videla <jorge.videla.v@gmail.com>
2025-05-13 16:01:54 -0400

    hash de password, update

--- api_usuarios/src/main/java/com/example/api_usuarios/controllers/UserController.java
+++ api_usuarios/src/main/java/com/example/api_usuarios/controllers/UserController.java
@@ -5,12 +5,14 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.api_usuarios.models.entities.User;
 import com.example.api_usuarios.models.requests.UserCreate;
+import com.example.api_usuarios.models.requests.UserUpdate;
 import com.example.api_usuarios.services.UserService;
 
 import jakarta.validation.Valid;
@@ -31,4 +33,10 @@ public class UserController {
     public User registrar(@Valid @RequestBody UserCreate body) {
         return userService.registrar(body);
     }
+
+    @PutMapping()
+    public User actualizar(@Valid @RequestBody UserUpdate body) {
+        return userService.actualizar(body);
+    }
+    
 }
--- api_usuarios/src/main/java/com/example/api_usuarios/services/UserService.java
+++ api_usuarios/src/main/java/com/example/api_usuarios/services/UserService.java
@@ -5,12 +5,16 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.web.server.ResponseStatusException;
 
 import com.example.api_usuarios.models.entities.User;
 import com.example.api_usuarios.models.requests.UserCreate;
+import com.example.api_usuarios.models.requests.UserUpdate;
 import com.example.api_usuarios.repositories.UserRepository;
+
+import jakarta.validation.Valid;
 
 @Service
 public class UserService {
@@ -31,12 +35,38 @@ public class UserService {
             //campos que vienen del cliente
             nuevoUsuario.setNombre(usuario.getNombre());
             nuevoUsuario.setEmail(usuario.getEmail());
-            nuevoUsuario.setPassword(usuario.getPassword());
+            nuevoUsuario.setPassword(hashearPassword(usuario.getPassword()));
             nuevoUsuario.setTelefono(usuario.getTelefono());
 
             return userRepository.save(nuevoUsuario);
         } catch (Exception e) {
             throw new ResponseStatusException(HttpStatus.BAD_REQUEST,"Usuario registrado");
         }
+    }
+
+    public String hashearPassword(String password) {
+        BCryptPasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
+        return passwordEncoder.encode(password); 
+    }
+
+    public User actualizar(UserUpdate body) {
+        User usuario = userRepository.findById(body.getId()).orElse(null);
+        if (usuario == null) {
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Usuario no encontrado");
+        }
+
+        if(body.getNombre() != null) {
+            usuario.setNombre(body.getNombre());
+        }
+        
+        if(body.getTelefono() != null) {
+            usuario.setTelefono(body.getTelefono());
+        }
+
+        if(body.getPassword() != null) {
+            usuario.setPassword(hashearPassword(body.getPassword()));
+        }
+
+        return userRepository.save(usuario);
     }
 }
